405d7ee5e0afdfe146d0f28c761998b31c36e560
Marlon Pina Tojal <dev@fnx.pt>
2024-10-23 08:57:24 +0200

    add new water fountain type

--- petkitaio/constants.py
+++ petkitaio/constants.py
@@ -270,7 +270,7 @@ BLE_HEADER = [-6, -4, -3]
 FEEDER_LIST = ['D3', 'D4', 'D4s', 'D4sh', 'Feeder', 'FeederMini']
 LITTER_LIST = ['T3', 'T4']
 PURIFIER_LIST = ['K2']
-WATER_FOUNTAIN_LIST = ['W5']
+WATER_FOUNTAIN_LIST = ['W5', 'Ctw3']
 
 LB_CMD_TO_KEY = {
     LitterBoxCommand.LIGHT_ON: LitterBoxCommandKey.START,
